3b747a340b89e649025d5bb62452e54c82ee9944
tenyoru <mail@tenyoru.io>
2026-09-21 16:55:17 +0200

    build: release version 0.4.1

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       qt = pkgs.qt6;
       cclock = pkgs.stdenv.mkDerivation {
         pname = "cclock";
-        version = "0.4.0";
+        version = "0.4.1";
         meta = {
           description = "Countdown timer overlay";
           mainProgram = "cclock";
--- src/main.cpp
+++ src/main.cpp
@@ -317,7 +317,7 @@ private:
 int main(int argc, char **argv) {
   QApplication app(argc, argv);
   app.setApplicationName("cclock");
-  app.setApplicationVersion("0.4.0");
+  app.setApplicationVersion("0.4.1");
   app.setDesktopFileName("cclock");
   app.setQuitOnLastWindowClosed(false);
   app.setWindowIcon(QIcon(":/qt/qml/CClock/cclock.svg"));
